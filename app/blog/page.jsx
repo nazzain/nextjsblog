60cce6baf53ea4729639914c17fee98013829d87
nazzain <135584640+nazzain@users.noreply.github.com>
2024-06-13 15:40:52 +0500

    url

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -4,7 +4,7 @@ import FirstBlog from "@/components/FirstBlog";
 import OtherBlogs from "@/components/OtherBlogs";
 
 async function fetchBlogs() {
-  const res = await fetch("api/blog", {
+  const res = await fetch("http://localhost:3000/api/blog", {
     cache: "no-store",
   });
 
