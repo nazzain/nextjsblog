ed454b8ab918a2f53ab3fdb0b8aeeb40ccb9431a
nazzain <135584640+nazzain@users.noreply.github.com>
2024-06-13 15:01:05 +0500

    build

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,11 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "./AuthProvider";
- const cors  = require('cors');
+//  const cors  = require('cors');
 
- app.use(cors({
-  origin:'http://localhost:3000/'
- }));
+//  app.use(cors({
+//   origin:'http://localhost:3000/'
+//  }));
 
 
 
